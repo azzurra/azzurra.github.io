4d458c0e4befc67ffa3ec65e827a1b616ffce265
Matteo Panella <morpheus@level28.org>
2015-02-01 22:49:06 +0100

    Current website as of 2015-02-01

---
+++ js/cse.js
@@ -0,0 +1,4 @@
+/*
+ * Code for Google Custom Search Engine
+ */
+(function(){var cx="012357419976593811995:q1itwatzbua",gcse=document.createElement("script");gcse.type="text/javascript",gcse.async=!0,gcse.src=(document.location.protocol=="https:"?"https:":"http:")+"//www.google.com/cse/cse.js?cx="+cx;var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(gcse,s)})();
